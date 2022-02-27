d0520f41e03622e045ddd1cc69283d48592c5517
Megan D <45672063+megan-d@users.noreply.github.com>
2022-02-26 21:13:00 -0700

    Add basic db config and domain model

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+db = SQLAlchemy(app)
 
 app = Flask(__name__)
